54e3fb51d9aa29be9affb406fa1349b2b8108805
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-11-26 00:33:36 +0530

    Initial Commit

---
+++ BookAnalyzt/BookAnalyztApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAnalyztApp.swift
+//  BookAnalyzt
+//
+//  Created by Bibin Jacob Pulickal on 26/11/20.
+//
+
+import SwiftUI
+
+@main
+struct BookAnalyztApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
